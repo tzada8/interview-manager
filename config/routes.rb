9300e7411ca54d9096b84a38a05089c4ca513cff
tzada8 <tzada@uwaterloo.ca>
2021-08-27 16:05:13 -0400

    updated path names

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :generics
   # resources :questions
   resources :interviews
-  get 'interviews/:id/questions', to: 'questions#index', as: 'show_questions'
-  get 'interviews/:id/questions/:id', to: 'questions#show', as: 'show_question'
+  get 'interviews/:id/questions', to: 'questions#index', as: 'questions'
+  get 'interviews/:id/questions/:id', to: 'questions#show', as: 'question'
 end
