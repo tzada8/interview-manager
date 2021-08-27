adb07ce48b4b26281ee5c0ce6415b888fdad6b08
tzada8 <tzada@uwaterloo.ca>
2021-08-27 16:01:28 -0400

    adding nested routes for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   resources :generics
-  resources :questions
+  # resources :questions
   resources :interviews
+  get 'interviews/:id/questions', to: 'questions#index', as: 'show_questions'
+  get 'interviews/:id/questions/:id', to: 'questions#show', as: 'show_question'
 end
