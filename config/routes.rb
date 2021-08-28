1ab36d1d66d2abfbef077c39832ab5b9432a37bf
tzada8 <tzada@uwaterloo.ca>
2021-08-28 13:13:01 -0400

    added basic about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
+  get '/about', to: 'static_pages#about', as: 'about'
   resources :generics
   resources :interviews do
     resources :questions
